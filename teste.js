498d979130fdf92dfe477691cbc93f7d7dfbff31
jorgedelisantos <deli64deli.jds@gmail.com>
2022-12-30 18:34:46 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,10 @@
+var idade = 18
+console.log(`voce tem ${idade} anos.`)
+if(idade  < 16){
+    console.log('seu voto é opcional')
+ } else if( idade < 18 || idade > 65 ){
+    console.log( 'voto ocional')
+ }else{
+    console.log(`voto obrigatório`)
+}
+
