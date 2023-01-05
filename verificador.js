9f5c423e544b82fa02f8a3a3b4999951b04053cc
jorgedelisantos <deli64deli.jds@gmail.com>
2023-01-04 21:52:15 -0300

    atualizado

--- verificador.js
+++ verificador.js
@@ -1,9 +1,9 @@
 function verificar(){
 var data = new Date()
-var ano = data.getFullYear()
-var forano = document.getElementById('txtano')
-var res = document.querySelector('div#res')
-if ( forano.value.length == 0 || forano.value > ano){
+ var ano = data.getFullYear()
+  var forano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+      if ( forano.value.length == 0 || forano.value > ano){
     window.alert ('[ERRO]Verique os dados e tente outra vez')
 }else{
      var formsex = document.getElementsByName('radsex')
@@ -34,16 +34,19 @@ if ( forano.value.length == 0 || forano.value > ano){
      }else if(formsex[1].checked){
         genero = 'mulher'
         if(idade >= 0 && idade < 10){
-            img.attributes('src','/fotos_js/mcrianca.png')
+            img.setAttribute('src','/fotos_js/mcrianca.png')
             //criança
-        }else if(idade < 21){
-            img.attributes('src', '/fotos_js/')
-            //jovem
-        }else if(idade < 50){
-            img.attributes('src', '/fotos_js/')
-            //adulto
-        }else{
 
+        }else if(idade < 30){
+            img.setAttribute('src', '/fotos_js/mjovemmulher.png')
+            //jovem
+
+        }else if(idade < 40){
+            img.setAttribute('src', '/fotos_js/mulheradulta.png')
+            //adulto
+
+        }else{
+            img.setAttribute('src', '/fotos_js/midadeidosa.png')
             //idosa
         }
      }
