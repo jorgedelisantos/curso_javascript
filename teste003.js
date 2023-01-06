a0da97828a248425445b42ff865297a8fcd89de3
jorgedelisantos <deli64deli.jds@gmail.com>
2023-01-05 21:43:23 -0300

    criei mais testes

---
+++ teste003.js
@@ -0,0 +1,9 @@
+var c = 1
+while (c <= 20){
+   c++
+console.log(`Total de: ${c} vezes`)
+
+}
+// Esse teste de REPETIÇÕES
+//Testa primeiramente o bloco
+//depois executa-o
---
+++ teste004.js
@@ -0,0 +1,9 @@
+var c = 1
+do{
+console.log(`Total de: ${c} vezes`)
+c++
+}while (c <= 9)
+
+// Esse teste de REPETIÇÕES
+// Executa o bloco primeiro
+// depois testa-o
