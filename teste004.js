11e8da8edae3b017aa7f73c01ff6b8995a972d4b
jorgedelisantos <deli64deli.jds@gmail.com>
2023-01-05 22:18:35 -0300

    criei um for

--- teste004.js
+++ teste004.js
@@ -1,9 +1,14 @@
-var c = 1
+/*var c = 1
 do{
 console.log(`Total de: ${c} vezes`)
 c++
 }while (c <= 9)
-
+*/
 // Esse teste de REPETIÇÕES
 // Executa o bloco primeiro
 // depois testa-o
+
+
+for ( c = 1 ; c <= 4 ; c++){
+console.log(c)
+}
